e6f1798dcec41763ff79bed484405085f26c7a7a
Nikita Kakade <81976504+nikitakakade16@users.noreply.github.com>
2025-11-27 05:37:53 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,13 @@
-scripts
+#!/bin/bash
+
+echo "Deploying static website..."
+
+WEBROOT="/var/www/html"
+SOURCE="/home/ec2-user/myapp"
+
+sudo rm -rf $WEBROOT/*
+sudo cp -r $SOURCE/* $WEBROOT/
+sudo chmod -R 755 $WEBROOT
+sudo systemctl restart httpd
+
+echo "Deployment completed!"
